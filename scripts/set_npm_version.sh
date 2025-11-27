1fcf4707dc2eb84121219e220ddb75a938fc378c
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-11-27 09:47:14 +0100

    Create script to update npm project version number

---
+++ scripts/set_npm_version.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+VERSION=$1
+
+echo -e "${GREEN}Version: ${VERSION}${NC}\n"
+
+echo -e "${YELLOW}📤 Setting version on platform packages...${NC}"
+find ./npm/platforms -name "package.json" -execdir sh -c "npm pkg set version=$VERSION" ";"
+
+pushd npm || exit
+
+pkgs=$(npm pkg get optionalDependencies | jq -r "keys | .[]")
+
+echo -e "\n${YELLOW}📤 Updating platform versions on main package...${NC}\n"
+for pkg in $pkgs; do
+  echo -e "${GREEN}Setting version for $pkg...${NC}"
+  npm pkg set "optionalDependencies.$pkg"="$VERSION"
+done
+
+echo -e "\n${YELLOW}📤 Setting version on main package...${NC}"
+npm pkg set version="$VERSION"
+
+popd || exit
+
+echo -e "\n${GREEN}🎉 All packages version set successfully!${NC}"
